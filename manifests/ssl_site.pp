e6322825b951c25a9982943e1e6e3fb7ec704370
Michael Watters <michael.watters@dart.biz>
2017-03-30 09:12:00 -0400

    New - Add nginx::ssl_site class
    Added nginx::ssl_site class to automate the installation of TLS
    certificates for nginx.

---
+++ manifests/ssl_site.pp
@@ -0,0 +1,32 @@
+# Install a configuration file for SSL sites
+
+class nginx::ssl_site (
+    String $site_name = $::fqdn,
+    Boolean $manage_firewall = true,
+    ) {
+
+    File {
+        owner  => 'nginx',
+        group  => 'nginx',
+        notify => Service['nginx'],
+    }
+
+    if $manage_firewall == true {
+        include 'dart::subsys::firewall::https'
+    }
+
+    file { "/etc/nginx/conf.d/${site_name}.ssl.conf":
+        content => template('nginx/ssl.conf.erb'),
+    }
+
+    file { "/etc/pki/tls/certs/${site_name}.crt":
+        source => "puppet:///modules/files/private/${site_name}/ssl/${site_name}.crt",
+        mode   => '0444',
+    }
+
+    file { "/etc/pki/tls/private/${site_name}.key":
+        source => "puppet:///modules/files/private/${site_name}/ssl/${site_name}.key",
+        mode   => '0440',
+    }
+
+}
